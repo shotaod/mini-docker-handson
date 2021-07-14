f50aead19157f1539ccba6db1eeed296f941a96e
ShotaOd <shota.oda+github@bizreach.co.jp>
2021-07-14 16:08:44 +0900

    [1-2] run arbitrary command from command line argument

--- commands/run.py
+++ commands/run.py
@@ -1,11 +1,17 @@
+import os
+from typing import List
+
 import linux
 
 
-def child_proc_callback():
-    print('hello world')
+def child_proc_callback(option: dict):
+    command = option['commands']
+    os.execvp(command[0], command)
 
 
-def exec_run():
-    # lesson 1.
+def exec_run(commands: List[str]):
     flags = 0
-    linux.clone(child_proc_callback, flags, ())
+    option = {'commands': commands}
+    pid = linux.clone(child_proc_callback, flags, (option,))
+
+    os.waitpid(pid, 0)
