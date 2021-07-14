51b9db1fa22fb01ee3af1446cf0e7d46a89f5510
ShotaOd <shota.oda+github@bizreach.co.jp>
2021-07-14 18:17:54 +0900

    [2-2] receive cpu option

--- commands/run.py
+++ commands/run.py
@@ -10,17 +10,19 @@ def child_proc_callback(option: dict):
     pid = os.getpid()
     print(f'pid: {pid}')
 
-    cg = cgroup.CGroup('hogehoge')
-    cg.set_cpu_limit(.25)
-    cg.add(pid)
+    cpu = option['cpu']
+    if cpu:
+        cg = cgroup.CGroup('hogehoge')
+        cg.set_cpu_limit(cpu)
+        cg.add(pid)
 
     command = option['commands']
     os.execvp(command[0], command)
 
 
-def exec_run(commands: List[str]):
+def exec_run(cpu: float, commands: List[str]):
     flags = 0
-    option = {'commands': commands}
+    option = {'cpu': cpu, 'commands': commands}
     pid = linux.clone(child_proc_callback, flags, (option,))
 
     os.waitpid(pid, 0)
