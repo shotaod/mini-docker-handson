d90a73c69b956bd34a602b003312299db545d2d6
natsumisawa <natsumi.sawa@bizreach.co.jp>
2022-03-05 09:27:18 +0000

    :tada: lesson1

--- commands/run.py
+++ commands/run.py
@@ -1,2 +1,18 @@
-def exec_run():
-    print(f'run command called!')
+import linux
+import os
+from typing import List
+
+def child_proc_callback(command: List[str]):
+    print(f"this is child proc!! pid: {os.getpid()}")
+    os.execvp(command[0], command)
+
+def exec_run(command: List[str]):
+    flags = 0
+    child_pid = linux.clone(
+        child_proc_callback,
+        flags,
+        (command, )
+    )
+
+    os.waitpid(child_pid, 0)
+
