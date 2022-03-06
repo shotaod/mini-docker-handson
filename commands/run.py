d2f89b5033659c9a23d018545e650d570a58b469
natsumisawa <natsumi.sawa@bizreach.co.jp>
2022-03-06 05:03:27 +0000

    :broom:

--- commands/run.py
+++ commands/run.py
@@ -3,11 +3,10 @@ import os
 from typing import List
 
 def child_proc_callback(command: List[str]):
-    print(f"this is child proc!! pid: {os.getpid()}")
+    print(f"container pid: {os.getpid()}")
     os.execvp(command[0], command)
 
 def exec_run(command: List[str]):
-    print(os.getpid())
     flags = 0
     child_pid = linux.clone(
         child_proc_callback,
