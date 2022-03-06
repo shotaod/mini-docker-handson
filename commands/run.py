064a07a23f31b75f43eba6530fba7a406b400e06
natsumisawa <natsumi.sawa@bizreach.co.jp>
2022-03-06 04:48:24 +0000

    :tada:

--- commands/run.py
+++ commands/run.py
@@ -1,18 +1,33 @@
 import linux
 import os
 from typing import List
+import commands.cgroup as cgroup
 
-def child_proc_callback(command: List[str]):
-    print(f"this is child proc!! pid: {os.getpid()}")
+def child_proc_callback(option: dict):
+    pid = os.getpid()
+    print(f"container pid: {pid}")
+    
+    linux.sethostname("container")
+    
+    cpu = option['cpu']
+    if cpu:
+    	cg = cgroup.CGroup('container')
+    	cg.set_cpu_limit(cpu)
+    	cg.add(pid)
+
+    command = option['command']
     os.execvp(command[0], command)
 
-def exec_run(command: List[str]):
-    print(os.getpid())
-    flags = 0
+
+def exec_run(cpu: float, command: List[str]):
+    flags = (linux.CLONE_NEWUTS | # UTS名前空間
+    	linux.CLONE_NEWPID # PID名前空間
+    )
+    option = {'cpu': cpu, 'command': command}
     child_pid = linux.clone(
         child_proc_callback,
         flags,
-        (command, )
+        (option, )
     )
 
     os.waitpid(child_pid, 0)
